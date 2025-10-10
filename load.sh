df024f817b07e3680ba29faf19c3f00d1e1618a8
JakubPawelczak <jakub.pawelczak.pl@gmail.com>
2025-10-10 13:20:08 +0000

    cleanup

--- load.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-#10 workers for 120 min
-echo "starting test of 10 workers for 120 min"
-
-kubectl run -i --tty --rm hey --image us-docker.pkg.dev/gke-demos-345619/hey/hey --restart=Never --  -c 10 -z 120m  http://vpa-demo-service
-
-#TODO: 100 requests during weekday, minimum requests during weekends
